2129df17f9d07d5ec7a9fbbaee5b97f6a8a7c8eb
Lil Doggy <u201923820@upc.edu.pe>
2026-05-25 00:15:23 -0500

    refactor(ui): layout, home and routing updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,18 @@ import { Home } from './shared/presentation/views/home/home';
 const about        = () => import('./shared/presentation/views/about/about.component').then(m => m.AboutComponent);
 const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent);
 const monitoring   = () => import('./monitoring/presentation/monitoring-routes').then(m => m.monitoringRoutes);
-const device       = () => import('./device/presentation/device-routes').then(m => m.deviceRoutes);
+const device = () => import('./device/presentation/device-routes').then((m) => m.deviceRoutes);
+const analytics = () => import('./analytics/presentation/analytics-routes').then(m => m.analyticsRoutes);
+const payment = () => import('./payment/presentation/payment.routes').then((m) => m.paymentRoutes);
 
 const baseTitle = 'Ventix';
 export const routes: Routes = [
-  { path: 'home',       component:     Home, title: `Home - ${baseTitle}` },
-  { path: 'monitoring', loadChildren:  monitoring },
-  { path: 'device',     loadChildren:  device },
-  { path: 'about',      loadComponent: about,                   title: `About - ${baseTitle}` },
-  { path: '',           redirectTo:    '/home',                  pathMatch: 'full' },
-  { path: '**',         loadComponent: pageNotFound,            title: `Page Not Found - ${baseTitle}` }
+  { path: 'home', component: Home, title: `Home - ${baseTitle}` },
+  { path: 'monitoring', loadChildren: monitoring },
+  { path: 'device', loadChildren: device },
+  { path: 'analytics', loadChildren: analytics },
+  { path: 'payment', loadChildren: payment },
+  { path: 'about', loadComponent: about, title: `About - ${baseTitle}` },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', loadComponent: pageNotFound, title: `Page Not Found - ${baseTitle}` },
 ];
